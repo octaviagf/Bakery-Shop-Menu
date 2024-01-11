aa234bfc879047cdf543606c2f816f65e7a7ef85
octaviagf <oagf0616@gmail.com>
2024-01-11 18:41:10 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const menu = [
+  {
+    name: "Sandwich de jamón y queso",
+    price: "$5",
+    photoName: "img/sandwich.jpg",
+  },
+  { name: "Pancakes", price: "$7", photoName: "img/pancakes.jpg" },
+  { name: "Huevos y bacon", price: "$10", photoName: "img/baconeggs.jpg" },
+  { name: "Americano", price: "$5", photoName: "img/americano.jpg" },
+  { name: "Espresso", price: "$7", photoName: "img/espresso.jpg" },
+  { name: "Latte", price: "$10", photoName: "img/latte.jpg" },
+  {
+    name: "Galletas de chispas de chocolate",
+    price: "$5",
+    photoName: "img/cookies.jpg",
+  },
+  { name: "Brownies", price: "$7", photoName: "img/brownies.jpg" },
+  { name: "Carlota de limón", price: "$10", photoName: "img/carlota.jpg" },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+    <div>
+      <Header />
+      <HeaderImg />
+      <Menu />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>La repostería de Dane</h1>
+    </header>
+  );
+}
+
+function HeaderImg() {
+  return (
+    <div className="headerimg">
+      <img src="img/brownies.jpg" alt="La repostería de Dane" className="img" />
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2 className="text">Our menu</h2>
+      <ul>
+        {menu.map((item) => (
+          <Items menuObj={item} key={item.name} />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+function Items(props) {
+  return (
+    <li className="item">
+      <div>
+        <img src={props.menuObj.photoName} alt={props.menuObj.name} />
+
+        <h2>{props.menuObj.name}</h2>
+        <p>{props.menuObj.price}</p>
+      </div>
+    </li>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
